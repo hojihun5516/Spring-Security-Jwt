ad9b5323078a05191351a14da247556ae9907ba9
hojihun5516 <hojihun5516@daum.net>
2023-05-06 02:43:40 +0900

    [Dto] User, UserProfile Dto 추가
    * 도메인 class에 해당하는 Dto를 추가한다
    
    close: #33
    Signed-off-by: hojihun5516 <hojihun5516@daum.net>

--- src/test/kotlin/com/example/springsecurityjwt/jwt/JwtTokenCreateServiceTest.kt
+++ src/test/kotlin/com/example/springsecurityjwt/jwt/JwtTokenCreateServiceTest.kt
@@ -2,22 +2,35 @@ package com.example.springsecurityjwt.jwt
 
 import com.example.springsecurityjwt.domains.User
 import com.example.springsecurityjwt.domains.UserProfile
+import com.example.springsecurityjwt.dtos.UserDto
+import com.example.springsecurityjwt.dtos.UserProfileDto
 import com.example.springsecurityjwt.support.Support
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class JwtTokenCreateServiceTest {
     @Test
-    fun `sut should create jwt token when user profile is given`() {
+    fun `sut should create jwt token when user profile dto is given`() {
         // Arrange
         val user = Support.fixture<User>()
         val userProfile = Support.fixture<UserProfile> {
             property(UserProfile::user) { user }
             property(UserProfile::userId) { user.id!! }
         }
+        val userProfileDto = UserProfileDto(
+            profileId = userProfile.id!!,
+            profileName = userProfile.name,
+            profileRole = userProfile.role,
+            user = UserDto(
+                userId = user.id!!,
+                username = user.username,
+                name = user.name,
+                birthday = user.birthday,
+            ),
+        )
 
         // Act
-        val actual = JwtTokenCreateService().createToken(userProfile)
+        val actual = JwtTokenCreateService().createToken(userProfileDto)
 
         // Assert
         assertThat(actual)
