e37aab1a9901932e56017414f7241c268d3fadba
moflow <32387358+hojihun5516@users.noreply.github.com>
2023-05-06 03:00:04 +0900

    [SignUp] SignUp 서비스 로직 추가
    * mapper를 생성하여 dto를 도메인으로 연결해준다
    * request dto는 외부와 통신하는 클래스이기 때문에 따로 mapper를 만들어서 사용한다
    
    work-items: #17

---
+++ src/main/kotlin/com/example/springsecurityjwt/services/SignUpService.kt
@@ -0,0 +1,29 @@
+package com.example.springsecurityjwt.services
+
+import com.example.springsecurityjwt.dtos.SignUpRequest
+import com.example.springsecurityjwt.dtos.UserProfileDto
+import com.example.springsecurityjwt.mappers.toUser
+import com.example.springsecurityjwt.repositories.UserProfileRepository
+import com.example.springsecurityjwt.repositories.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class SignUpService(
+    private val userProfileRepository: UserProfileRepository,
+    private val userRepository: UserRepository,
+) {
+    fun signUp(signUpRequest: SignUpRequest): UserProfileDto {
+        checkUserExists(username = signUpRequest.username)
+
+        val user = userRepository.save(signUpRequest.toUser())
+
+        val userProfile = userProfileRepository.save(user.toUserRoleProfile())
+
+        return UserProfileDto.from(userProfile)
+    }
+
+    private fun checkUserExists(username: String) {
+        // TODO: custom exception
+        userRepository.findByUsername(username)?.let { throw Exception("already exists username") }
+    }
+}
