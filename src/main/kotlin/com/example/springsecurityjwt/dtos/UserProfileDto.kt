ebef6e27810ed2db4786eaa11810cf1f8c704e1b
moflow <32387358+hojihun5516@users.noreply.github.com>
2023-05-06 02:57:31 +0900

    [Dto] 정적 팩토리 메서드 추가
    * UserDto, UserProfileDto에 정적 팩토리 메서드를 추가하여 entity를 받아 생성하게한다
    * Service 로직 내부에서 사용할 Dto다
    
    work-items: #17

--- src/main/kotlin/com/example/springsecurityjwt/dtos/UserProfileDto.kt
+++ src/main/kotlin/com/example/springsecurityjwt/dtos/UserProfileDto.kt
@@ -1,10 +1,22 @@
 package com.example.springsecurityjwt.dtos
 
 import com.example.springsecurityjwt.domains.Role
+import com.example.springsecurityjwt.domains.UserProfile
 
 data class UserProfileDto(
     val profileId: Long,
     val profileName: String,
     val profileRole: Role,
     val user: UserDto
-)
+) {
+    companion object {
+        fun from(userProfile: UserProfile): UserProfileDto {
+            return UserProfileDto(
+                profileId = userProfile.id!!,
+                profileName = userProfile.name,
+                profileRole = userProfile.role,
+                user = UserDto.from(userProfile.user),
+            )
+        }
+    }
+}
