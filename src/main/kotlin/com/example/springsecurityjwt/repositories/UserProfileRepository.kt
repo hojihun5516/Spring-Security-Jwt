5ae6e7c20ffddda2ab669c49e04da48e154612be
moflow <32387358+hojihun5516@users.noreply.github.com>
2023-05-08 01:55:07 +0900

    [SignIn] profile find 함수 추가
    work-items: #39

--- src/main/kotlin/com/example/springsecurityjwt/repositories/UserProfileRepository.kt
+++ src/main/kotlin/com/example/springsecurityjwt/repositories/UserProfileRepository.kt
@@ -1,6 +1,10 @@
 package com.example.springsecurityjwt.repositories
 
+import com.example.springsecurityjwt.domains.Role
 import com.example.springsecurityjwt.domains.UserProfile
+import com.example.springsecurityjwt.dtos.UserId
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserProfileRepository:JpaRepository<UserProfile, Long>
+interface UserProfileRepository : JpaRepository<UserProfile, Long> {
+    fun findByUserIdAndRole(userId: UserId, role: Role): UserProfile?
+}
