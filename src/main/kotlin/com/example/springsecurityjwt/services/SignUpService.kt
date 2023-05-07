b2b1c629216cb7b6230c8af6df9812f54be061a6
moflow <32387358+hojihun5516@users.noreply.github.com>
2023-05-08 02:02:38 +0900

    [SignUn] SignUp transactional 추가
    close: #41

--- src/main/kotlin/com/example/springsecurityjwt/services/SignUpService.kt
+++ src/main/kotlin/com/example/springsecurityjwt/services/SignUpService.kt
@@ -6,8 +6,10 @@ import com.example.springsecurityjwt.mappers.toUser
 import com.example.springsecurityjwt.repositories.UserProfileRepository
 import com.example.springsecurityjwt.repositories.UserRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class SignUpService(
     private val userProfileRepository: UserProfileRepository,
     private val userRepository: UserRepository,
