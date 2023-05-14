ae82c725c7a6674ff90154f18a22851b6bcba3d9
moflow <hojihun5516@daum.net>
2023-05-14 18:40:18 +0900

    [SpringSecuriyConfig] CustomAuthenticationProvider 추가
    close: #51

--- src/main/kotlin/com/example/springsecurityjwt/configs/SpringSecurityConfig.kt
+++ src/main/kotlin/com/example/springsecurityjwt/configs/SpringSecurityConfig.kt
@@ -1,18 +1,27 @@
 package com.example.springsecurityjwt.configs
 
+import com.example.springsecurityjwt.services.CustomAuthenticationProvider
+import com.example.springsecurityjwt.services.CustomUserDetailsService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
 @EnableWebSecurity
-class SpringSecurityConfig {
+class SpringSecurityConfig(
+    private val userDetailsService: CustomUserDetailsService,
+    private val passwordEncoder: PasswordEncoder,
+) {
 
     /**
      * 인증/인가 검사를 수행하지 않도록 설정
@@ -46,5 +55,15 @@ class SpringSecurityConfig {
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
         return http.build()
+    }
+
+    @Bean
+    fun authenticationProvider(): AuthenticationProvider? {
+        return CustomAuthenticationProvider(userDetailsService, passwordEncoder)
+    }
+
+    @Bean
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager? {
+        return authenticationConfiguration.authenticationManager
     }
 }
