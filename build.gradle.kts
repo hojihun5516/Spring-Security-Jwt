f864f9c4725511ca7c20946eef28a0d99ddd3997
moflow <hojihun5516@daum.net>
2023-05-23 20:47:28 +0900

    [Test] springmockk 의존성 추가
    * https://github.com/Ninja-Squad/springmockk
    * 가장 최신 버전 4.0.2
    
    close: #76

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     testImplementation("io.mockk:mockk:1.13.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 }
 
 tasks.withType<KotlinCompile> {
