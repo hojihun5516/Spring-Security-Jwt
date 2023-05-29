d713153999fc4ddc18819d288d70f2bf0496367b
moflow <hojihun5516@daum.net>
2023-05-29 16:50:02 +0900

    [ErrorResult] 패키지 위치 변경
    close: #97

--- src/main/kotlin/com/example/springsecurityjwt/configs/CustomControllerAdvice.kt
+++ src/main/kotlin/com/example/springsecurityjwt/configs/CustomControllerAdvice.kt
@@ -1,6 +1,7 @@
 package com.example.springsecurityjwt.configs
 
 import com.example.springsecurityjwt.exceptions.BaseException
+import com.example.springsecurityjwt.exceptions.ErrorResult
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
